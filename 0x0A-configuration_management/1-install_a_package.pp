8c260331f97e87f93671b81de11ddb52b5bbe0fb
programmingwithjerry <eneyellowj@gmail.com>
2024-06-28 18:53:52 +0000

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,25 @@
 # To create a Puppet manifest that installs Flask version 2.1.0 using pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# ensure that python is available
+package { 'python3.8':
+  ensure => present,
 }
 
+# ensure that pip is available
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Flask 
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# Install Werkzeug 
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
