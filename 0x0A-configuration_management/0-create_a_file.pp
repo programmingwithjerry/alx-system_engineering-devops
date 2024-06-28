945fd76f4455995732632ebe5d3bd8bd7dfd9186
programmingwithjerry <eneyellowj@gmail.com>
2024-06-28 18:40:15 +0000

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-file { '/tmp/school':
-  ensure  => 'file',
-  content => 'I love Puppet',
+file { 'school':
+  ensure  => present,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet',
 }
-
